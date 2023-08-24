103dae3b3bcc765c55b75ea0e368af54e0f08797
RLVictor <vityad888@yandex.ru>
2023-08-24 20:27:10 +0300

    Initial Commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Привет")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Привет"));
+    }
+}
